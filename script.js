ae213f61504d3cf73b637fd706932ab233a10873
Anamika <avikajha@Avikas-iMac.local>
2021-07-28 20:47:03 -0700

    added logic for searching  book and populating in console.log

---
+++ script.js
@@ -0,0 +1,35 @@
+function initPage() {
+const searchTerm = document.getElementById("searchTerm");
+const searchEl = document.getElementById("submit");
+
+searchEl.addEventListener("click", function () {
+console.log(searchTerm.value);
+fetchBooks();
+})
+}
+initPage();
+
+// fetch('https://www.googleapis.com/books/v1/volumes?q='+ searchTerm).then(function(response){
+//     return response.json();
+// })
+// .then(function(data){
+//     console.log(data)
+// });
+
+
+function fetchBooks(){
+    fetch('https://www.googleapis.com/books/v1/volumes?q='+ searchTerm).then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        console.log(data)
+    });
+}
+
+
+
+
+
+
+
+
