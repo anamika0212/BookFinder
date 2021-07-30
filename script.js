66730f5bebad09c94c012e0892792dea2a0092c6
saboagye23 <85259473+saboagye23@users.noreply.github.com>
2021-07-30 13:48:15 -0700

    Adding header and searchbox style

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
 function initPage() {
-const searchTerm = document.getElementById("searchTerm");
-const searchEl = document.getElementById("submit");
-
-searchEl.addEventListener("click", function () {
-console.log(searchTerm.value);
-fetchBooks();
-})
-}
-initPage();
-
-// fetch('https://www.googleapis.com/books/v1/volumes?q='+ searchTerm).then(function(response){
-//     return response.json();
-// })
-// .then(function(data){
-//     console.log(data)
-// });
-
-
-function fetchBooks(){
-    fetch('https://www.googleapis.com/books/v1/volumes?q='+ searchTerm).then(function(response){
-        return response.json();
+    const searchTerm = document.getElementById("searchTerm");
+    const searchEl = document.getElementById("submit");
+    
+    searchEl.addEventListener("click", function () {
+    console.log(searchTerm.value);
+    fetchBooks();
     })
-    .then(function(data){
-        console.log(data)
-    });
-}
+    }
+    initPage();
+    
+    // fetch('https://www.googleapis.com/books/v1/volumes?q='+ searchTerm).then(function(response){
+    //     return response.json();
+    // })
+    // .then(function(data){
+    //     console.log(data)
+    // });
+    
+    
+    function fetchBooks(){
+        fetch('https://www.googleapis.com/books/v1/volumes?q='+ searchTerm).then(function(response){
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data)
+        });
+    }
+    
+    
+    
 
 
 
